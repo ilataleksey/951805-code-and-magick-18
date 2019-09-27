48a75e03549bdee5338e1fcbb76aba0784bc69be
ilataleksey <ilataleksey@gmail.com>
2019-09-27 19:17:44 +0300

    Добавляет функцию getRandomInt(), выносит повторяющиеся формулы для определения координат и размеров элементов статистики в отдельные переменные, исправляет ошибку в имени переменной-константы

--- js/stat.js
+++ js/stat.js
@@ -8,7 +8,7 @@ var GAP = 10;
 var BAR_GAP = 50;
 var TEXT_WIDTH = 16;
 var BAR_WIDTH = 40;
-var barHeight = 150;
+var BAR_HEIGHT = 150;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -31,21 +31,31 @@ window.renderStatistics = function (ctx, names, times) {
 
   ctx.fillStyle = '#000000';
 
-  // ctx.font = '16px, PT Mono';
+  ctx.font = '16px, PT Mono';
 
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP * 4);
   ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP * 4 + TEXT_WIDTH);
 
   var maxTime = getMaxElement(times);
 
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
   for (var i = 0; i < names.length; i++) {
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'hsl(225, ' + Math.random() * 100 + '%, 50%)';
+      ctx.fillStyle = 'hsl(225, ' + getRandomInt(1, 100) + '%, 50%)';
     }
-    ctx.fillText(names[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT);
-    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - TEXT_WIDTH, BAR_WIDTH, -(barHeight * times[i] / maxTime));
-    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - TEXT_WIDTH - GAP - (barHeight * times[i] / maxTime));
+
+    var statisticsX = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i;
+    var barHeight = -(BAR_HEIGHT * times[i] / maxTime);
+
+    ctx.fillText(names[i], statisticsX, CLOUD_HEIGHT);
+    ctx.fillRect(statisticsX, CLOUD_HEIGHT - TEXT_WIDTH, BAR_WIDTH, barHeight);
+    ctx.fillText(Math.round(times[i]), statisticsX, CLOUD_HEIGHT - TEXT_WIDTH - GAP + barHeight);
   }
 };
