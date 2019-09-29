3b36dc05142cf0c8ddd88a40375a73be087ce337
ilataleksey <ilataleksey@gmail.com>
2019-09-29 18:37:10 +0300

    Добавляет функцию для определения цвета игрока и функцию для описания текста в поле статистики

--- js/stat.js
+++ js/stat.js
@@ -25,31 +25,35 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var renderText = function (ctx, x, y, color, font, texts) {
+  ctx.fillStyle = color;
+  ctx.font = font;
+
+  for (var i = 0; i < texts.length; i++) {
+    ctx.fillText(texts[i], x, y + TEXT_WIDTH * i);
+  }
+};
+
+var getRandomInt = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var getColorForName = function (name) {
+  return name === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(225, ' + getRandomInt(1, 100) + '%, 50%)';
+};
+
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
 
-  ctx.fillStyle = '#000000';
-
-  ctx.font = '16px, PT Mono';
-
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP * 4);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP * 4 + TEXT_WIDTH);
+  renderText(ctx, CLOUD_X + GAP, CLOUD_Y + GAP * 4, '#000000', '16px, PT Mono', ['Ура вы победили!', 'Список результатов:']);
 
   var maxTime = getMaxElement(times);
 
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
   for (var i = 0; i < names.length; i++) {
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'hsl(225, ' + getRandomInt(1, 100) + '%, 50%)';
-    }
+    ctx.fillStyle = getColorForName(names[i]);
 
     var statisticsX = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i;
     var barHeight = -(BAR_HEIGHT * times[i] / maxTime);
